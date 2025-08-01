1a3b4ea09dfe8bfd49bcef03e319267264291478
Adriics <adriancalvachesanchez@gmail.com>
2025-08-01 16:49:31 +0200

    new: A simple Welcome Page

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,8 @@
+export const HomePage = () => {
+  return (
+    <main className="p-6">
+      <h1 className="text-3xl font-bold">Bienvenido a tu app ✨</h1>
+      <p className="mt-2 text-gray-600">¡Todo empieza aquí!</p>
+    </main>
+  );
+};
